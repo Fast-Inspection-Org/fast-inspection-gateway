4aab409dd357f43ed90c04f5560053cb3a312de4
Adrian <adrian.suarez.alphen@gmail.com>
2024-08-22 22:17:31 -0400

    Rutas para el cambio de contraseña de un Usuario, implementadas en el módulo de users-auth

--- src/users-auth/users-auth.controller.ts
+++ src/users-auth/users-auth.controller.ts
@@ -7,8 +7,6 @@ import { NameUsersAuth } from 'src/utils/global';
 import { LoginDTO } from './dto/login-dto';
 import { firstValueFrom } from 'rxjs';
 import { ActivacionDTO } from './dto/activacion.dto';
-import { FiltersUsuarioDTO } from './dto/filters-usuario.dto';
-
 
 @Controller('users-auth')
 export class UsersAuthController {
@@ -34,6 +32,16 @@ export class UsersAuthController {
     }
   }
 
+  @Post('verificarCodigoIdentidad')
+  public async verificarCodigoIdentidad(@Body() activacionDTO: ActivacionDTO) {
+    try {
+      return await firstValueFrom(this.usersAuthClient.send('verificarCodigoIdentidad', activacionDTO))
+    } catch (error) { // siempre en caso de error, este será un RpcExpection
+      const rpcError: RpcError = error
+      throw new HttpException(rpcError.message, rpcError.status)
+    }
+  }
+
   @Get("getAllUsers/:idSolicitante") // el parámetro "idSolicitante" representa el indentificador el usuario (administrador) que ejecutó la petición
   public async findAll(@Param("idSolicitante") idSolicitante: String, @Query("nombre") nombre: string, @Query("rol") rol: RolEnum) {
 
@@ -50,7 +58,7 @@ export class UsersAuthController {
   }
 
   @Get('getUsuarioById/:id')
-  public async findOne(@Param('id') id: string) {
+  public async findOneById(@Param('id') id: string) {
     try {
       return await firstValueFrom(this.usersAuthClient.send('getUsuarioById', id))
     } catch (error) { // siempre en caso de error, este será un RpcExpection
@@ -59,7 +67,20 @@ export class UsersAuthController {
     }
   }
 
-  
+  @Get('getUsuario')
+  public async findOne(@Query("idUsuario") idUsuario: string, @Query("nombreUsuario") nombreUsuario: string, @Query("rol") rol: RolEnum, @Query("email") email: string) {
+    try {
+      return await firstValueFrom(this.usersAuthClient.send('getUsuario', {
+        _id: idUsuario,
+        nombreUsuario: nombreUsuario,
+        email: email,
+        rol: rol
+      }))
+    } catch (error) { // siempre en caso de error, este será un RpcExpection
+      const rpcError: RpcError = error
+      throw new HttpException(rpcError.message, rpcError.status)
+    }
+  }
 
   @Patch('updateUser/:id')
   public async update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
@@ -99,6 +120,26 @@ export class UsersAuthController {
   public async registrer(@Body() userDTO: CreateUsuarioDto) {
     try {
       return await firstValueFrom(this.usersAuthClient.send('registrer', userDTO))
+    } catch (error) { // siempre en caso de error, este será un RpcExpection
+      const rpcError: RpcError = error
+      throw new HttpException(rpcError.message, rpcError.status)
+    }
+  }
+
+  @Post('cambiarContrasena')
+  public async cambiarContrasena(@Body() payload: { idUsuario: string, newContrasena: string, contrasenaAnterior?: string }) {
+    try {
+      return await firstValueFrom(this.usersAuthClient.send('cambiarContrasena', payload))
+    } catch (error) { // siempre en caso de error, este será un RpcExpection
+      const rpcError: RpcError = error
+      throw new HttpException(rpcError.message, rpcError.status)
+    }
+  }
+
+  @Post('enviarCodigoVerificacionIdentidad/:idUsuario')
+  public async enviarCodigoVerificacionIdentidad(@Param("idUsuario") idUsuario: string) {
+    try {
+      return await firstValueFrom(this.usersAuthClient.send('enviarCodigoVerificacionIdentidad', idUsuario))
     } catch (error) { // siempre en caso de error, este será un RpcExpection
       const rpcError: RpcError = error
       throw new HttpException(rpcError.message, rpcError.status)
