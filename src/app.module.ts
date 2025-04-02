20ab582bff37474ee5a2a00914935645b8149c5b
Ailen003 <ailendelgado007@gmail.com>
2025-04-02 18:25:05 -0400

    feat: deleted app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigsModule } from './configs/configs.module';
 import { UsersAuthModule } from './users-auth/users-auth.module';
 import { InspeccionModule } from './inspection-controller/inspeccion.module';
 import { EdificationsControllerModule } from './edifications-controller/edifications-controller.module';
 
-
 @Module({
-  imports: [ConfigsModule, InspeccionModule, UsersAuthModule, EdificationsControllerModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigsModule,
+    InspeccionModule,
+    UsersAuthModule,
+    EdificationsControllerModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
