9cf12f50bf3ad05cd75eeb2655e13f807fc13579
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-03 16:16:12 -0400

    docs(herramientas): implemented swagger documentation structure in the route data of the herramientas

--- src/configs/herramienta-analisis-criticidad/dto/update-herramienta-analisis-criticidad.dto.ts
+++ src/configs/herramienta-analisis-criticidad/dto/update-herramienta-analisis-criticidad.dto.ts
@@ -1,3 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UpdateHerramientaAnalisisCriticidadDTO {
-    nombre: string
+  @ApiProperty({
+    description: 'Representa el nombre de la herramienta',
+    example: 'Herramienta de Análisis de Criticidad',
+    type: 'texto',
+    required: true,
+  })
+  nombre: string;
 }
--- src/configs/herramientas/dto/herramienta.dto.ts
+++ src/configs/herramientas/dto/herramienta.dto.ts
@@ -1,3 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { CampoDTO } from 'src/configs/campo/dto/campo.dto';
+
 export class HerramientaDTO {
-    
+  @ApiProperty({
+    description: 'Representa el nombre de la herramienta',
+    example: 'Herramienta de Análisis de Criticidad',
+    type: 'texto',
+    required: true,
+  })
+  nombre: string;
+  tipo: string;
+  @ApiProperty({
+    description:
+      'Representa la configuración a la cual pertenece dicha herramienta',
+    example: '{"version": 1}',
+    type: 'json',
+    required: true,
+  })
+  config: { version: number }; // atributo que representa la configuracion a la que pertenece la herramienta
 }
