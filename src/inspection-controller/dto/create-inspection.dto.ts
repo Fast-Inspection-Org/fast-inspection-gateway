45945e91e089cced032432366a848c23bcd74f36
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-03 18:00:45 -0400

    docs(inspections): implemented swagger documentation structure in the route data of the inspections

--- src/inspection-controller/dto/create-inspection.dto.ts
+++ src/inspection-controller/dto/create-inspection.dto.ts
@@ -1,7 +1,29 @@
-import { CreateDeterioroDTO } from "../deterioro/dto/create-deterioro.dto"
+import { ApiProperty } from '@nestjs/swagger';
+import { CreateDeterioroDTO } from '../deterioro/dto/create-deterioro.dto';
+import { deteriorosExample } from './examples-dto';
 
 export class CreateInspectionDTO {
-    configId: string // representa la configuración con la que fue realiza la inspección
-    edificacionId: string
-    deterioros: Array<CreateDeterioroDTO>
+  @ApiProperty({
+    description:
+      'Representa el indentificador de la configuración con la que fue realizada la inspección',
+    example: '1',
+    type: 'texto',
+    required: true,
+  })
+  configId: string; // representa la configuración con la que fue realiza la inspección
+  @ApiProperty({
+    description:
+      'Representa el indentificador de la edificación a la cual se le realizó la inspección',
+    example: '1',
+    type: 'texto',
+    required: true,
+  })
+  edificacionId: string;
+  @ApiProperty({
+    description: 'Representa los deterioros identificados en la inspección',
+    example: deteriorosExample,
+    type: 'json',
+    required: true,
+  })
+  deterioros: Array<CreateDeterioroDTO>;
 }
---
+++ src/inspection-controller/dto/examples-dto.ts
@@ -0,0 +1,16 @@
+export const deteriorosExample = `[
+    {
+      "codigo": "C-02",
+      "sistemaId": 175,
+      "subsistemaId": 102,
+      "materialId": 111,
+      "tipoDeterioroId": 107,
+      "campos": [
+        {
+          "nombre": "descripción",
+          "tipo": "Numérico",
+          "valor": "Un deterioro polémico"
+        }
+      ]
+    }
+  ]`;
