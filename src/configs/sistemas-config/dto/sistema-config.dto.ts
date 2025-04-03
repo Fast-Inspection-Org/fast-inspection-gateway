1788ca59ae202cf7b48ef723b4726d1fcc952f0f
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-03 11:34:25 -0400

    docs(sistemas-config): implemented swagger documentation structure in the route data of the sistemas configs

--- src/configs/sistemas-config/dto/sistema-config.dto.ts
+++ src/configs/sistemas-config/dto/sistema-config.dto.ts
@@ -1,14 +1,32 @@
-import { ConfigDTO } from "src/configs/dto/config.dto"
-import { HerramientaDTO } from "src/configs/herramientas/dto/herramienta.dto"
-import { SubsistemaConfigDTO } from "src/configs/subsistemas-config/dto/subsistema-config.dto"
-
+import { ApiProperty } from '@nestjs/swagger';
+import { ConfigDTO } from 'src/configs/dto/config.dto';
+import { HerramientaDTO } from 'src/configs/herramientas/dto/herramienta.dto';
+import { SubsistemaConfigDTO } from 'src/configs/subsistemas-config/dto/subsistema-config.dto';
 
 export class SistemaConfigDTO {
-    id: number
-    nombre: String
-    subSistemasConfig?: Array<SubsistemaConfigDTO>
-    herramienta: HerramientaDTO // atributo que define la herramienta a la cual pertenece el sistema
-    config: ConfigDTO // atributo que define a la configuracion que pertenece el sistemaConfig
-
-   
+  id: number;
+  @ApiProperty({
+    description: 'Representa el nombre del sistema de configuración',
+    example: 'Estructural',
+    type: 'texto',
+    required: true,
+  })
+  nombre: String;
+  subSistemasConfig?: Array<SubsistemaConfigDTO>;
+  @ApiProperty({
+    description:
+      'Representa la Herramienta seleccionada para ser usada en este sistema',
+    example: '{id: 2}',
+    type: 'json',
+    required: true,
+  })
+  herramienta: HerramientaDTO; // atributo que define la herramienta a la cual pertenece el sistema
+  @ApiProperty({
+    description:
+      'Representa la Configuración a la que pertenecerá dicho sistema',
+    example: '{id: 2}',
+    type: 'json',
+    required: true,
+  })
+  config: ConfigDTO; // atributo que define a la configuracion que pertenece el sistemaConfig
 }
--- src/configs/sistemas-config/dto/update-sistema-config.dto.ts
+++ src/configs/sistemas-config/dto/update-sistema-config.dto.ts
@@ -1,8 +1,28 @@
-import { HerramientaDTO } from "src/configs/herramientas/dto/herramienta.dto"
-
+import { ApiProperty } from '@nestjs/swagger';
+import { HerramientaDTO } from 'src/configs/herramientas/dto/herramienta.dto';
 
 export class UpdateSistemaConfigDTO {
-    nombre: String
-    herramienta: HerramientaDTO // atributo que define la herramienta a la cual pertenece el sistema
-    configVersion: number // atributo de vital importancia para saber que sistema actualizar
+  @ApiProperty({
+    description: 'Representa el nombre del sistema de configuración',
+    example: 'Estructural',
+    type: 'texto',
+    required: true,
+  })
+  nombre: String;
+  @ApiProperty({
+    description:
+      'Representa la Herramienta seleccionada para ser usada en este sistema',
+    example: '{id: 2}',
+    type: 'json',
+    required: true,
+  })
+  herramienta: HerramientaDTO; // atributo que define la herramienta a la cual pertenece el sistema
+  @ApiProperty({
+    description:
+      'Representa la versión de la configuración a la cual pertenece dicho sistema',
+    example: '1',
+    type: 'numérico',
+    required: true,
+  })
+  configVersion: number; // atributo de vital importancia para saber que sistema actualizar
 }
