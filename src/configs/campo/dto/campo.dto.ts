e728a4e364ad90952505b6e4c356ab19c451cf50
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-03 15:24:16 -0400

    docs(campos): implemented swagger documentation structure in the route data of the campos

--- src/configs/campo/dto/campo.dto.ts
+++ src/configs/campo/dto/campo.dto.ts
@@ -1,9 +1,36 @@
-import { HerramientaDTO } from "src/configs/herramientas/dto/herramienta.dto"
+import { ApiProperty } from '@nestjs/swagger';
+import { HerramientaDTO } from 'src/configs/herramientas/dto/herramienta.dto';
 
 export class CampoDTO {
-    id: number
-    nombre: String
-    nivelImportancia: number
-    configVersion: number // indica la version a la que pertence el campo
-    herramientaAnalisisCriticidad?: HerramientaDTO // Atributo que define a la herramienta analisis de criticidad que pertenece el campo
+  id: number;
+  @ApiProperty({
+    description: 'Representa el nombre del campo afectado',
+    example: 'Salud',
+    type: 'texto',
+    required: true,
+  })
+  nombre: String;
+  @ApiProperty({
+    description:
+      'Representa el nivel de importancia de dicho campo afectado, el cual es un atributo que define que tan importante es este campo para una edificación',
+    example: '1',
+    type: 'numérico',
+    required: true,
+  })
+  nivelImportancia: number;
+  @ApiProperty({
+    description:
+      'Representa la versión de la configuración a la cual pertenece dicho campo',
+    example: '1',
+    type: 'numérico',
+    required: true,
+  })
+  configVersion: number; // indica la version a la que pertence el campo
+  @ApiProperty({
+    description: 'Representa la herramienta a la cual pertenece dicho campo',
+    example: '{"id": 1}',
+    type: 'json',
+    required: true,
+  })
+  herramientaAnalisisCriticidad?: HerramientaDTO; // Atributo que define a la herramienta analisis de criticidad que pertenece el campo
 }
--- src/configs/campo/dto/update-campo.dto.ts
+++ src/configs/campo/dto/update-campo.dto.ts
@@ -1,4 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UpdateCampoDTO {
-    nombre: String
-    nivelImportancia: number
+  @ApiProperty({
+    description: 'Representa el nombre del campo afectado',
+    example: 'Salud',
+    type: 'texto',
+    required: true,
+  })
+  nombre: String;
+  @ApiProperty({
+    description:
+      'Representa el nivel de importancia de dicho campo afectado, el cual es un atributo que define que tan importante es este campo para una edificación',
+    example: '1',
+    type: 'numérico',
+    required: true,
+  })
+  nivelImportancia: number;
 }
