33004e055f0b35b889b409f691c0e06efeb72ed6
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-15 19:12:52 -0400

    feat(campo-dto): add CampoDTOHerramienta class for handling tool field data

--- src/configs/campo/dto/campo.dto.ts
+++ src/configs/campo/dto/campo.dto.ts
@@ -34,3 +34,29 @@ export class CampoDTO {
   })
   herramientaAnalisisCriticidad?: HerramientaDTO; // Atributo que define a la herramienta analisis de criticidad que pertenece el campo
 }
+
+export class CampoDTOHerramienta {
+  @ApiProperty({
+    description: 'Representa el nombre del campo afectado',
+    example: 'Salud',
+    type: 'texto',
+    required: true,
+  })
+  nombre: String;
+  @ApiProperty({
+    description:
+      'Representa el nivel de importancia de dicho campo afectado, el cual es un atributo que define que tan importante es este campo para una edificación',
+    example: '1',
+    type: 'numérico',
+    required: true,
+  })
+  nivelImportancia: number;
+  @ApiProperty({
+    description:
+      'Representa la versión de la configuración a la cual pertenece dicho campo',
+    example: '1',
+    type: 'numérico',
+    required: true,
+  })
+  configVersion: number; // indica la version a la que pertence el campo
+}
