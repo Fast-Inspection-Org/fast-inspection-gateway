f86f2e3625fce87a2bbf28d68e318f520ab59f45
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-16 23:50:08 -0400

    fix(campo-herramienta-dto): standardize import quotes and add newline at end of file

--- src/configs/campo/dto/campo-herramienta.dto.ts
+++ src/configs/campo/dto/campo-herramienta.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CampoDTOHerramienta {
   @ApiProperty({
@@ -24,4 +24,22 @@ export class CampoDTOHerramienta {
     required: true,
   })
   configVersion: number; // indica la version a la que pertence el campo
+}
+
+export class CampoDTOHerramientaEdit {
+  @ApiProperty({
+    description: 'Representa el nombre del campo afectado',
+    example: 'Salud',
+    type: 'texto',
+    required: true,
+  })
+  nombre: String;
+  @ApiProperty({
+    description:
+      'Representa el nivel de importancia de dicho campo afectado, el cual es un atributo que define que tan importante es este campo para una edificación',
+    example: '1',
+    type: 'numérico',
+    required: true,
+  })
+  nivelImportancia: number;
 }
